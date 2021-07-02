67b54329650ca29c4fdb17d172d5867ab9a5bb37
Winnie Xiao <winniex@stanford.edu>
2021-07-02 22:25:47 +0200

    functional backend

--- app.js
+++ app.js
@@ -1,39 +1,72 @@
 const express = require("express")
 const Web3 = require('web3');
-const fs = require('fs');
-const solc = require('solc');
+const contract = require("@0xcert/ethereum-erc721/build/nf-token.json");
+const {PORT, NETWORK, KEY, INFURA_KEY, CONTRACT_ADDRESS, ADDRESS} = require("./config.js")
 
+// Start express app
 const app = express()
-const port = 3000
-const ADDRESS = "0x0B41992E9A47Ba2257699a1826F5f994f761EbC7"
 
-// deploys the ERC721 contract to Rinkeby testnet
-app.get('/', (req, res) => {
-    // Connect to local Ethereum node
-    const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+// Connect to local Ethereum node
+const web3 = new Web3(new Web3.providers.HttpProvider(`https://${NETWORK}.infura.io/v3/${INFURA_KEY}`));
+const abi = contract.NFToken.abi
+const bytecode = '0x' + contract.NFToken.evm.bytecode.object
 
-    // Compile the source code
-    const input = fs.readFileSync('./Token.sol');
-    const output = solc.compile(input.toString(), 1);
-    const bytecode = output.contracts[':Token'].bytecode;
-    const abi = JSON.parse(output.contracts[':Token'].interface);
-    
+const deployContract = () => {
+    const signer = web3.eth.accounts.privateKeyToAccount(KEY)
+    web3.eth.accounts.wallet.add(signer)
+
     // Create and deploy contract object
-    const contract = new web3.eth.Contract(abi);
-    contract.deploy({
-      data: '0x' + bytecode,
-    })
-    .send({
-        from: ADDRESS,
-        gas: 5000000,
+    const Instance = new web3.eth.Contract(abi);
+    Instance.options.data = bytecode
+    const deployTx = Instance.deploy()
+
+    // NEED TO RETURN so that a promise can be returned to caller
+    return deployTx.send({
+        from: signer.address,
+        gas: 14237245,
     })
     .then((newContractInstance) => {
-        console.log(newContractInstance.options.address) // instance with the new contract address
-        res.status(200).send(newContractInstance.options.address)
+        return newContractInstance.options.address
     })
-    .catch(err => res.status(500).send({ error: 'something blew up' }))
+    .catch(err => {
+        console.log(err)
+        return err
+    })
+}
+
+// Deploys basic 0xcert/ERC721 contract to NETWORK
+app.get('/', (req, res) => {
+    deployContract()
+        .then((address) => {
+            res.send({contractAddress: address})
+        })
+        .catch((err) => {
+            console.log("err" + err)
+            res.send({err: err})
+        })
 })
 
-app.listen(port, () => {
-    console.log(`Contract Factory listening at http://127.0.0.1:${port}`)
+// creates a token
+app.get('/balanceOf', (req, res) => {
+    const contractAddress = req.query.contractAddress ? req.query.contractAddress : CONTRACT_ADDRESS
+    const accountAddress = req.query.accountAddress ? req.query.accountAddress : ADDRESS
+    
+    const Instance = new web3.eth.Contract(abi, contractAddress, {
+        from: ADDRESS
+    })
+    Instance.options.address = contractAddress
+
+    Instance.methods.balanceOf(accountAddress)
+        .call({ from: accountAddress })
+        .then(result => res.send(result))
+        .catch(err => res.send(err))
+})
+
+app.get('/getTransactionCount', (req, res) => {
+    web3.eth.getTransactionCount(ADDRESS)
+        .then(num => res.send({num : num}))
+})
+
+app.listen(PORT, () => {
+    console.log(`Contract Factory listening at http://127.0.0.1:${PORT}`)
 })
