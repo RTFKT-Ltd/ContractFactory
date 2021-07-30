aa15676bb2a8327a460409b57c5e757d1eeece22
Winnie Xiao <winniex@stanford.edu>
2021-07-30 16:13:55 +0200

    add revert reason to mint

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const fs = require("fs");
-const solc = require("solc");
 const Web3 = require("web3");
+const getRevertReason = require("eth-revert-reason");
 const {
   PORT,
   NETWORK,
@@ -10,6 +9,8 @@ const {
   CONTRACT_ADDRESS,
   ADDRESS,
 } = require("./config.js");
+
+const { instantiateContract } = require("./utils.js");
 
 // Start express app
 const app = express();
@@ -20,40 +21,14 @@ const web3 = new Web3(
     `https://${NETWORK}.infura.io/v3/${INFURA_KEY}`
   )
 );
-const LATEST_TOKEN_ID = 2;
-
-const instantiateContract = () => {
-  const sources = {};
-  compileImports("./Base.sol", sources);
-
-  var input = {
-    language: "Solidity",
-    sources: sources,
-    settings: {
-      outputSelection: {
-        "*": {
-          "*": ["*"],
-        },
-      },
-    },
-  };
-
-  const output = solc.compile(JSON.stringify(input));
-  const contract = JSON.parse(output);
-  const bytecode =
-    "0x" + contract.contracts["./Base.sol"]["Base"].evm.bytecode.object;
-  const abi = contract.contracts["./Base.sol"]["Base"].abi;
-  return {
-    bytecode: bytecode,
-    abi: abi,
-  };
-};
+web3.eth.handleRevert = true;
+const LATEST_TOKEN_ID = 123;
 
 const deployContract = () => {
   const signer = web3.eth.accounts.privateKeyToAccount(KEY);
   web3.eth.accounts.wallet.add(signer);
 
-  const metadata = instantiateContract();
+  const metadata = instantiateContract("./Base.sol");
   // Create and deploy contract object
   const Instance = new web3.eth.Contract(metadata.abi);
   Instance.options.data = metadata.bytecode;
@@ -74,77 +49,7 @@ const deployContract = () => {
     });
 };
 
-// returns sources: { "Contract.sol": { content: fs.readFileSync("pathName.sol",utf8)...}}
-const compileImports = (root, sources) => {
-  sources[root] = { content: fs.readFileSync(root, "utf8") };
-  const imports = getNeededImports(root);
-  for (let i = 0; i < imports.length; i++) {
-    compileImports(imports[i], sources);
-  }
-  // {Base.sol: {content...}}
-};
-
-// returns all the import paths in absolute
-const getNeededImports = (path) => {
-  const file = fs.readFileSync(path, "utf8");
-  const files = new Array();
-  file
-    .toString()
-    .split("\n")
-    .forEach(function (line, index, arr) {
-      if (
-        (index === arr.length - 1 && line === "") ||
-        !line.trim().startsWith("import")
-      ) {
-        return;
-      }
-      // the import is legit
-      const relativePath = line.substring(8, line.length - 2);
-      const fullPath = buildFullPath(path, relativePath);
-      files.push(fullPath);
-    });
-  return files;
-};
-
-// parent: node_modules/.../ERC721/ERC721.sol
-// path: ../../etc
-const buildFullPath = (parent, path) => {
-  let curDir = parent.substr(0, parent.lastIndexOf("/")); //i.e. ./node/.../ERC721
-  if (path.startsWith("./")) {
-    return curDir + "/" + path.substr(2);
-  }
-
-  while (path.startsWith("../")) {
-    curDir = curDir.substr(0, curDir.lastIndexOf("/"));
-    path = path.substr(3);
-  }
-
-  return curDir + "/" + path;
-};
-
-app.get("/", (req, res) => {
-  const sources = {};
-  compileImports("./Base.sol", sources);
-
-  var input = {
-    language: "Solidity",
-    sources: sources,
-    settings: {
-      outputSelection: {
-        "*": {
-          "*": ["*"],
-        },
-      },
-    },
-  };
-
-  const output = solc.compile(JSON.stringify(input));
-  const contract = JSON.parse(output);
-  //   const bytecode = output.contracts["Token"].bytecode;
-  //   const abi = JSON.parse(output.contracts["Token"].interface);
-
-  res.send(contract.contracts);
-});
+app.get("/", (req, res) => {});
 
 // Deploys basic 0xcert/ERC721 contract to NETWORK
 app.get("/deploy", (req, res) => {
@@ -153,7 +58,7 @@ app.get("/deploy", (req, res) => {
       res.send({ contractAddress: address });
     })
     .catch((err) => {
-      console.log("err" + err);
+      console.log("err", err);
       res.send({ err: err });
     });
 });
@@ -183,32 +88,49 @@ app.get("/getTransactionCount", (req, res) => {
   web3.eth.getTransactionCount(ADDRESS).then((num) => res.send({ num: num }));
 });
 
-app.get("/mint", (req, res) => {
-  const tokenId = req.query.tokenId ? req.query.tokenId : LATEST_TOKEN_ID;
-  const contractAddress = req.query.contractAddress;
+app.get("/mint", async (req, res, next) => {
+  if (!req.query.to) {
+    const err = new Error('Required query param "to" missing');
+    err.status = 400;
+    next(err);
+  }
+  if (!req.query.tokenId) {
+    const err = new Error('Required query param "tokenId" missing');
+    err.status = 400;
+    next(err);
+  }
+  if (!req.query.contractAddress) {
+    const err = new Error('Required query param "contractAddress" missing');
+    err.status = 400;
+    next(err);
+  }
+
+  const { to, tokenId, contractAddress } = req.query;
 
   const signer = web3.eth.accounts.privateKeyToAccount(KEY);
   web3.eth.accounts.wallet.add(signer);
 
-  const metadata = instantiateContract();
+  const metadata = instantiateContract("./Base.sol");
   const contract = new web3.eth.Contract(metadata.abi, contractAddress);
-  console.log("Contract", contract);
-  contract.methods
-    .mint("0x3b634db3a35da1488aeafb18f1be9108d8408e2c", 0)
-    // needed to use signer instead of string "0x...", otherwise, result in Error: The method eth_sendTransaction does not exist/is not available
-    .send({ from: signer.address, gas: 14237245 })
-    .on("transactionHash", function (hash) {
-      console.log("hash", hash);
-    })
-    .on("receipt", function (receipt) {
-      console.log("receipt", receipt);
-    })
-    .on("confirmation", function (confirmationNumber, receipt) {
-      console.log("confirmation", confirmationNumber);
-    })
-    .on("error", function (error, receipt) {
-      console.log("error", error);
-    });
+  try {
+    await contract.methods
+      .mint(to, tokenId)
+      // needed to use signer instead of string "0x...", otherwise, result in Error: The method eth_sendTransaction does not exist/is not available
+      .send({ from: signer.address, gas: 14237245 })
+      .on("transactionHash", function (hash) {
+        console.log("tx hash", hash);
+      })
+      .on("receipt", function (receipt) {
+        console.log("receipt", receipt);
+      })
+      .on("confirmation", function (confirmationNumber) {
+        console.log("confirmation", confirmationNumber);
+      })
+      .then((receipt) => res.send(receipt));
+  } catch (e) {
+    console.log(e);
+    res.send({ error: e.reason });
+  }
 });
 
 app.listen(PORT, () => {
---
+++ utils.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+const solc = require("solc");
+
+// returns sources: { "Contract.sol": { content: fs.readFileSync("pathName.sol",utf8)...}}
+// using recursion
+const compileImports = (root, sources) => {
+  sources[root] = { content: fs.readFileSync(root, "utf8") };
+  const imports = getNeededImports(root);
+  for (let i = 0; i < imports.length; i++) {
+    compileImports(imports[i], sources);
+  }
+};
+
+// returns all the import paths in absolute path
+const getNeededImports = (path) => {
+  const file = fs.readFileSync(path, "utf8");
+  const files = new Array();
+  file
+    .toString()
+    .split("\n")
+    .forEach(function (line, index, arr) {
+      if (
+        (index === arr.length - 1 && line === "") ||
+        !line.trim().startsWith("import")
+      ) {
+        return;
+      }
+      // the import is legit
+      const relativePath = line.substring(8, line.length - 2);
+      const fullPath = buildFullPath(path, relativePath);
+      files.push(fullPath);
+    });
+  return files;
+};
+
+// parent: node_modules/.../ERC721/ERC721.sol
+// returns absolute path of a relative one using the parent path
+const buildFullPath = (parent, path) => {
+  let curDir = parent.substr(0, parent.lastIndexOf("/")); //i.e. ./node/.../ERC721
+  if (path.startsWith("./")) {
+    return curDir + "/" + path.substr(2);
+  }
+
+  while (path.startsWith("../")) {
+    curDir = curDir.substr(0, curDir.lastIndexOf("/"));
+    path = path.substr(3);
+  }
+
+  return curDir + "/" + path;
+};
+
+const instantiateContract = (baseContractPath) => {
+  const sources = {};
+  compileImports(baseContractPath, sources);
+
+  var input = {
+    language: "Solidity",
+    sources: sources,
+    settings: {
+      outputSelection: {
+        "*": {
+          "*": ["*"],
+        },
+      },
+    },
+  };
+
+  const output = solc.compile(JSON.stringify(input));
+  const contract = JSON.parse(output);
+  const bytecode =
+    "0x" + contract.contracts[baseContractPath]["Base"].evm.bytecode.object;
+  const abi = contract.contracts[baseContractPath]["Base"].abi;
+  return {
+    bytecode: bytecode,
+    abi: abi,
+  };
+};
+
+module.exports = {
+  instantiateContract,
+};
